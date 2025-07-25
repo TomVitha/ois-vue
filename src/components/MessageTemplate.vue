db01bf97527e112b9903c483b9f0ba404f79eee2
Tom Vitha <tomasvitha01@gmail.com>
2025-07-25 13:16:33 +0200

    Messenger cards layout

--- src/components/MessageTemplate.vue
+++ src/components/MessageTemplate.vue
@@ -3,27 +3,41 @@
     from: string
     to?: string
     datetime: string
-    subject?: string
+    subject: string
   }>()
 </script>
 
 <template>
-  <div class="chat card-body">
-    <RouterLink to="/komunikace">Zavřít</RouterLink>
-    <h2 class="mb-4">{{ subject }}</h2>
-    <div class="d-flex justify-content-between">
-      <div class="d-flex">
-        <div>
-          <div class="font-weight-medium">Od: {{ from }}</div>
-          <!-- <div class="text-secondary">Komu: {{ to }}</div> -->
+  <div class="card">
+    <div class="card-header gap-2">
+      <div class="btn-actions">
+        <RouterLink to="/komunikace" class="btn btn-action d-lg-none">
+          <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
+            <path d="M18 6l-12 12"></path>
+            <path d="M6 6l12 12"></path>
+          </svg> -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
+            <path d="M15 6l-6 6l6 6"></path>
+          </svg>
+        </RouterLink>
+      </div>
+      <div class="space-y-1 w-100">
+        <h2 class="m-0">{{ subject }}</h2>
+        <div class="d-flex justify-content-between">
+          <div>
+            <div class="font-weight-medium">Od: {{ from }}</div>
+            <div v-if="to" class="text-secondary">Komu: {{ to }}</div>
+          </div>
+          <div>
+            <span class="text-secondary fs-5">{{ datetime }}</span>
+          </div>
         </div>
       </div>
-      <div>
-        <span class="text-secondary fs-5">{{ datetime }}</span>
-      </div>
     </div>
-    <div class="markdown mt-5">
-      <slot></slot>
+    <div class="card-body">
+      <div class="markdown">
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
--- src/layouts/LayoutMessenger.vue
+++ src/layouts/LayoutMessenger.vue
@@ -1,103 +1,125 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
   import NavbarVertical from '../components/NavbarVertical.vue'
   import Navbar from '../components/Navbar.vue'
   import Footer from '../components/Footer.vue'
+
+  // TEMP/DEV - to test many messages and page overflow
+  // onMounted(() => {
+  //   const el = document.querySelector('.nav-link[href="/msg-2"]')
+  //   console.log(el)
+  //   if (el && el.parentNode) {
+  //     for (let i = 0; i < 15; i++) {
+  //       el.parentNode.appendChild(el.cloneNode(true))
+  //     }
+  //   }
+  // })
 </script>
 
 <template>
   <NavbarVertical />
   <Navbar />
   <div class="page-wrapper">
-    <div class="card flex-fill messenger border-top-0 border-start-0 border-end-0 rounded-0">
-      <div class="row g-0 flex-fill">
-        <div class="col-12 col-lg-auto d-flex flex-column border-end messenger__list" >
-          <div class="card-header space-y-2 align-items-stretch ">
-            <!-- Compose button -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#new-message-modal">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
-                <path d="M13.5 6.5l4 4" />
-              </svg>
-              Nová zpráva
-            </button>
-            <!-- Compose message window -->
-            <div class="modal" id="new-message-modal" tabindex="-1">
-              <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="page-body">
+      <div class="container-fluid flex-fill space-y-0">
+        <div class="messenger row row-deck row-cards flex-fill">
+          <!-- Messages list column -->
+          <div class="col-12 col-lg-auto d-flex flex-column messenger__list">
+            <div class="card">
+              <div class="card-header space-y-2 align-items-stretch ">
+                <!-- Compose button -->
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#new-message-modal">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
+                    <path d="M13.5 6.5l4 4" />
+                  </svg>
+                  Nová zpráva
+                </button>
+                <!-- Compose message window -->
+                <div class="modal" id="new-message-modal" tabindex="-1">
+                  <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
+                        beatae delectus
+                        deleniti dolorem eveniet facere fuga iste nemo nesciunt nihil odio
+                        perspiciatis, quia quis
+                        reprehenderit sit tempora totam unde.
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+                      </div>
+                    </div>
                   </div>
-                  <div class="modal-body">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-                    beatae delectus
-                    deleniti dolorem eveniet facere fuga iste nemo nesciunt nihil odio
-                    perspiciatis, quia quis
-                    reprehenderit sit tempora totam unde.
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
-                  </div>
+                </div>
+                <!-- Search bar -->
+                <div class="input-icon">
+                  <span class="input-icon-addon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
+                      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+                      <path d="M21 21l-6 -6"></path>
+                    </svg>
+                  </span>
+                  <input type="text" value="" class="form-control" placeholder="Hledat…" aria-label="Hledat">
+                </div>
+              </div>
+              <div class="card-body p-0 scrollable flex-fill">
+                <div class="nav flex-column nav-pills">
+                  <RouterLink to="/msg-1" class="nav-link text-start mw-100 px-4 py-3" aria-selected="false">
+                    <div class="row align-items-center flex-fill mw-100">
+                      <div class="col text-body">
+                        <div class="d-flex align-items-center justify-content-between">
+                          <span class="text-truncate">Odesilatel 1</span>
+                          <div class="d-flex align-items-center gap-2 fs-5">
+                            <i class="ti ti-paperclip"></i>
+                            <span class="text-secondary">11:45</span>
+                          </div>
+                        </div>
+                        <div class="fs-5 text-truncate w-100">Title (msg-1)</div>
+                        <div class="d-flex align-items-center justify-content-between gap-2">
+                          <span class="text-secondary fs-5 text-truncate">obsah zprávy</span>
+                          <i class="ti ti-star text-secondary fs-3"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </RouterLink>
+                  <RouterLink to="/msg-2" class="nav-link text-start mw-100 px-4 py-3" aria-selected="false">
+                    <div class="row align-items-center flex-fill mw-100">
+                      <div class="col text-body">
+                        <div class="d-flex align-items-center justify-content-between">
+                          <span class="text-truncate">Odesilatel 2</span>
+                          <div class="d-flex align-items-center gap-2 fs-5">
+                            <i class="ti ti-paperclip"></i>
+                            <span class="text-secondary">11:45</span>
+                          </div>
+                        </div>
+                        <div class="fs-5 text-truncate w-100">Title (msg-2)</div>
+                        <div class="d-flex align-items-center justify-content-between gap-2">
+                          <span class="text-secondary fs-5 text-truncate">obsah zprávy</span>
+                          <i class="ti ti-star text-secondary fs-3"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </RouterLink>
                 </div>
               </div>
             </div>
-            <!-- Search bar -->
-            <div class="input-icon">
-              <span class="input-icon-addon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                  <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-                  <path d="M21 21l-6 -6"></path>
-                </svg>
-              </span>
-              <input type="text" value="" class="form-control" placeholder="Hledat…" aria-label="Hledat">
-            </div>
           </div>
-          <div class="card-body p-0 scrollable flex-fill">
-            <div class="nav flex-column nav-pills" role="tablist">
-              <RouterLink to="/msg-1" class="nav-link text-start mw-100 px-4 py-3" aria-selected="false">
-                <div class="row align-items-center flex-fill mw-100">
-                  <div class="col text-body">
-                    <div class="d-flex align-items-center justify-content-between">
-                      <span class="text-truncate">Odesilatel 1</span>
-                      <div class="d-flex align-items-center gap-2 fs-5">
-                        <i class="ti ti-paperclip"></i>
-                        <span class="text-secondary">11:45</span>
-                      </div>
-                    </div>
-                    <div class="fs-5 text-truncate w-100">Title (msg-1)</div>
-                    <div class="d-flex align-items-center justify-content-between gap-2">
-                      <span class="text-secondary fs-5 text-truncate">obsah zprávy</span>
-                      <i class="ti ti-star text-secondary fs-3"></i>
-                    </div>
-                  </div>
-                </div>
-              </RouterLink>
-              <RouterLink to="/msg-2" class="nav-link text-start mw-100 px-4 py-3" aria-selected="false">
-                <div class="row align-items-center flex-fill mw-100">
-                  <div class="col text-body">
-                    <div class="d-flex align-items-center justify-content-between">
-                      <span class="text-truncate">Odesilatel 2</span>
-                      <div class="d-flex align-items-center gap-2 fs-5">
-                        <i class="ti ti-paperclip"></i>
-                        <span class="text-secondary">11:45</span>
-                      </div>
-                    </div>
-                    <div class="fs-5 text-truncate w-100">Title (msg-2)</div>
-                    <div class="d-flex align-items-center justify-content-between gap-2">
-                      <span class="text-secondary fs-5 text-truncate">obsah zprávy</span>
-                      <i class="ti ti-star text-secondary fs-3"></i>
-                    </div>
-                  </div>
-                </div>
-              </RouterLink>
-            </div>
+          <!-- Message body column -->
+          <div class="col d-flex flex-column messenger__body">
+            <!-- <div class="card"> -->
+              <!-- <div class="card-body"> -->
+                <!-- Default Slot = RouterView = Message body -->
+                <slot />
+              <!-- </div> -->
+            <!-- </div> -->
           </div>
-        </div>
-        <!-- Default Slot = RouterView = Message body -->
-        <div class="col d-flex flex-column messenger__body">
-          <slot />
         </div>
       </div>
     </div>
