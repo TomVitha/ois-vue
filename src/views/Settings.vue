21682002c3665a5d4772e84d414170fee6ae5bf5
Tom Vitha <tomasvitha01@gmail.com>
2025-12-12 13:29:02 +0100

    Settings improvements

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -61,18 +61,21 @@
 
           <!-- TODO: Předělat oznámení, pokud má klient aktivní podanou žádost -->
 
+          <!-- * Osobní údaje osob klienta -->
           <div class="col-12">
             <div class="card card-md" id="osobni-udaje">
+              <div class="card-header">
+                <h3 class="card-title">Osoby klienta</h3>
+              </div>
               <div class="card-body">
-                <h3 class="card-title">Osobní údaje</h3>
                 <div class="datagrid">
                   <div class="datagrid-item">
-                    <div class="datagrid-title">Klient</div>
+                    <div class="datagrid-title">Jméno</div>
                     <div>Adam Novák</div>
                   </div>
                   <div class="datagrid-item">
-                    <div class="datagrid-title">Adresa</div>
-                    <div>Skopičkova 748/9, Praha 4, 143 00</div>
+                    <div class="datagrid-title">Trvalý pobyt</div>
+                    <div>Pryskyřicova 304/11, Praha 0, 100 00</div>
                   </div>
                   <div class="datagrid-item">
                     <div class="datagrid-title">Rodinný stav</div>
@@ -80,20 +83,58 @@
                   </div>
                   <div class="datagrid-item">
                     <div class="datagrid-title">Email</div>
-                    <div>novak@seznam.cz</div>
+                    <div>adam.novak88@seznam.cz</div>
                   </div>
                   <div class="datagrid-item">
                     <div class="datagrid-title">Telefon</div>
                     <div>+420721584630</div>
                   </div>
-                  <div class="datagrid-item">
+                  <!-- <div class="datagrid-item">
                     <div class="datagrid-title">IČ</div>
                     <div>38171674</div>
-                  </div>
-                  <div class="datagrid-item">
+                  </div> -->
+                  <!-- <div class="datagrid-item">
                     <div class="datagrid-title">DIČ</div>
                     <div>CZ8706080086</div>
+                  </div> -->
+                </div>
+                <div class="mt-4">
+                  <div class="btn-list">
+                    <!-- Odkáže na stránku nových žádostí a otevře rovnou formulář této žádosti / Nebo otevře formulář rovnou zde -->
+                    <RouterLink to="/zadost-nova" class="btn btn-1">Zažádat o změnu</RouterLink>
                   </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="datagrid">
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Jméno</div>
+                    <div>Eva Nováková</div>
+                  </div>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Trvalý pobyt</div>
+                    <div>Pryskyřicova 304/11, Praha 0, 100 00</div>
+                  </div>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Rodinný stav</div>
+                    <div>Ženatá</div>
+                  </div>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Email</div>
+                    <div>evavolexoxo@seznam.cz</div>
+                  </div>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Telefon</div>
+                    <div>+420606459301</div>
+                  </div>
+                  <!-- <div class="datagrid-item">
+                    <div class="datagrid-title">IČ</div>
+                    <div>38171674</div>
+                  </div> -->
+                  <!-- <div class="datagrid-item">
+                    <div class="datagrid-title">DIČ</div>
+                    <div>CZ8706080086</div>
+                  </div> -->
                 </div>
                 <div class="mt-4">
                   <div class="btn-list">
@@ -104,95 +145,10 @@
               </div>
             </div>
           </div>
+
+          <!-- * Kontaktní email -->
           <div class="col-12">
             <div class="card card-md" id="heslo">
-              <div class="card-body">
-                <h3 class="card-title">Heslo</h3>
-                <p class="card-subtitle">
-                  Zde můžete změnit heslo ke svému účtu.
-                </p>
-                <div>
-                  <a href="#dialog-password-change" class="btn" data-bs-toggle="modal" data-bs-target="#dialog-password-change">
-                    Změnit
-                  </a>
-                </div>
-              </div>
-
-              <!-- password change dialog -->
-              <div class="modal fade" tabindex="-1" id="dialog-password-change">
-                <div class="modal-dialog modal-dialog-centered modal-md modal-fullscreen-sm-down" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title">Změna hesla</h5>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      <div class="row gy-3">
-                        <div class="row-12">
-                          <label class="form-label d-inline-block" for="current-password">Aktuální heslo</label>
-                          <div class="input-group input-group-flat">
-                            <input :type="passwordVisible ? 'text' : 'password'" id="current-password" name="current-password" autocomplete="current-password" class="form-control fs-4" placeholder="" required>
-                            <span class="input-group-text">
-                              <a href="#" class="link-secondary" data-bs-toggle="tooltip" aria-label="Přepnout viditelnost hesla" data-bs-original-title="Přepnout viditelnost hesla" @click.prevent="togglePasswordVisibility()">
-                                <i class="fs-2 icon" :class="['ti', passwordVisible ? 'ti-eye-off' : 'ti-eye']"></i>
-                              </a>
-                            </span>
-                          </div>
-                        </div>
-                        <div class="row-12">
-                          <label class="form-label d-inline-block" for="new-password-1">Nové heslo</label>
-                          <div class="input-group input-group-flat">
-                            <input :type="passwordVisible ? 'text' : 'password'" id="new-password-1" name="new-password-1" autocomplete="new-password" class="form-control fs-4" placeholder="" required>
-                            <span class="input-group-text">
-                              <a href="#" class="link-secondary" data-bs-toggle="tooltip" aria-label="Přepnout viditelnost hesla" data-bs-original-title="Přepnout viditelnost hesla" @click.prevent="togglePasswordVisibility()">
-                                <i class="fs-2 icon" :class="['ti', passwordVisible ? 'ti-eye-off' : 'ti-eye']"></i>
-                              </a>
-                            </span>
-                          </div>
-                        </div>
-                        <div class="row-12">
-                          <label class="form-label d-inline-block" for="new-password-2">Potvrďtě nové heslo</label>
-                          <div class="input-group input-group-flat">
-                            <input :type="passwordVisible ? 'text' : 'password'" id="new-password-2" name="new-password-2" autocomplete="new-password" class="form-control fs-4" placeholder="" required>
-                            <span class="input-group-text">
-                              <a href="#" class="link-secondary" data-bs-toggle="tooltip" aria-label="Přepnout viditelnost hesla" data-bs-original-title="Přepnout viditelnost hesla" @click.prevent="togglePasswordVisibility()">
-                                <i class="fs-2 icon" :class="['ti', passwordVisible ? 'ti-eye-off' : 'ti-eye']"></i>
-                              </a>
-                            </span>
-                          </div>
-                        </div>
-                        <!-- NOTE: CHECKLIST SE BUDE DYNAMICKY AKTUALIZOVAT JAK HO UŽIVATEL PÍŠE PRO OKAMŽITOU ODEZVU -->
-                        <div class="row-12">
-                          <div>
-                            <ul class="space-y-1">
-                              <li class="text-success">
-                                Minimum 8 znaků
-                              </li>
-                              <li class="text-success">
-                                1 velké písmeno
-                              </li>
-                              <li class="text-secondary">
-                                1 číslo
-                              </li>
-                              <li class="text-secondary">
-                                1 speciální znak
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn me-auto" data-bs-dismiss="modal">Zrušit</button>
-                      <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Uložit</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-12">
-            <div class="card card-md" id="upozorneni-plateb">
               <div class="card-body">
                 <h3 class="card-title">Upozornění blížících se plateb</h3>
                 <p class="card-subtitle">
@@ -217,11 +173,112 @@
                   </select>
                 </div>
               </div>
+              <div class="card-body">
+                <h3 class="card-title">Kontaktní email</h3>
+                <p class="card-subtitle">
+                  Váš hlavní kontaktní email, na které vám budou chodit upozornění.
+                </p>
+                <div class="row">
+                  <div class="col-md-6">
+                    <input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder" value="novakovi@seznam.cz" disabled></input>
+                  </div>
+                </div>
+                <div class="btn-list mt-4">
+                  <a href="#dialog-email-change" class="btn" data-bs-toggle="modal" data-bs-target="#dialog-email-change">
+                    Změnit
+                  </a>
+                </div>
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Heslo</h3>
+                <p class="card-subtitle">
+                  Zde můžete změnit heslo ke svému účtu.
+                </p>
+                <div class="btn-list">
+                  <a href="#dialog-password-change" class="btn" data-bs-toggle="modal" data-bs-target="#dialog-password-change">
+                    Změnit
+                  </a>
+                </div>
+              </div>
             </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- * Password change Dialog -->
+    <div class="modal fade" tabindex="-1" id="dialog-password-change">
+      <div class="modal-dialog modal-dialog-centered modal-md modal-fullscreen-sm-down" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Změna hesla</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="row gy-3">
+              <div class="row-12">
+                <label class="form-label d-inline-block" for="current-password">Aktuální heslo</label>
+                <div class="input-group input-group-flat">
+                  <input :type="passwordVisible ? 'text' : 'password'" id="current-password" name="current-password" autocomplete="current-password" class="form-control fs-4" placeholder="" required>
+                  <span class="input-group-text">
+                    <a href="#" class="link-secondary" data-bs-toggle="tooltip" aria-label="Přepnout viditelnost hesla" data-bs-original-title="Přepnout viditelnost hesla" @click.prevent="togglePasswordVisibility()">
+                      <i class="fs-2 icon" :class="['ti', passwordVisible ? 'ti-eye-off' : 'ti-eye']"></i>
+                    </a>
+                  </span>
+                </div>
+              </div>
+              <div class="row-12">
+                <label class="form-label d-inline-block" for="new-password-1">Nové heslo</label>
+                <div class="input-group input-group-flat">
+                  <input :type="passwordVisible ? 'text' : 'password'" id="new-password-1" name="new-password-1" autocomplete="new-password" class="form-control fs-4" placeholder="" required>
+                  <span class="input-group-text">
+                    <a href="#" class="link-secondary" data-bs-toggle="tooltip" aria-label="Přepnout viditelnost hesla" data-bs-original-title="Přepnout viditelnost hesla" @click.prevent="togglePasswordVisibility()">
+                      <i class="fs-2 icon" :class="['ti', passwordVisible ? 'ti-eye-off' : 'ti-eye']"></i>
+                    </a>
+                  </span>
+                </div>
+              </div>
+              <div class="row-12">
+                <label class="form-label d-inline-block" for="new-password-2">Potvrďtě nové heslo</label>
+                <div class="input-group input-group-flat">
+                  <input :type="passwordVisible ? 'text' : 'password'" id="new-password-2" name="new-password-2" autocomplete="new-password" class="form-control fs-4" placeholder="" required>
+                  <span class="input-group-text">
+                    <a href="#" class="link-secondary" data-bs-toggle="tooltip" aria-label="Přepnout viditelnost hesla" data-bs-original-title="Přepnout viditelnost hesla" @click.prevent="togglePasswordVisibility()">
+                      <i class="fs-2 icon" :class="['ti', passwordVisible ? 'ti-eye-off' : 'ti-eye']"></i>
+                    </a>
+                  </span>
+                </div>
+              </div>
+              <!-- NOTE: CHECKLIST SE BUDE DYNAMICKY AKTUALIZOVAT JAK HO UŽIVATEL PÍŠE PRO OKAMŽITOU ODEZVU -->
+              <div class="row-12">
+                <div>
+                  <ul class="space-y-1">
+                    <li class="text-success">
+                      Minimum 8 znaků
+                    </li>
+                    <li class="text-success">
+                      1 velké písmeno
+                    </li>
+                    <li class="text-secondary">
+                      1 číslo
+                    </li>
+                    <li class="text-secondary">
+                      1 speciální znak
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn me-auto" data-bs-dismiss="modal">Zrušit</button>
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Uložit</button>
           </div>
         </div>
       </div>
     </div>
+
   </PageTemplate>
 
 </template>
