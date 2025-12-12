090e84b6600b072457df471b317974f3f6513aaf
Tom Vitha <tomasvitha01@gmail.com>
2025-12-12 13:36:50 +0100

    Apartment detail infobox designing

--- src/views/Apt.vue
+++ src/views/Apt.vue
@@ -35,8 +35,8 @@
         </div>
       </div>
     </template> -->
-    
-    <div class="row row-cards row-deck">
+
+    <div class="row row-cards">
 
       <!-- <div class="col-12">
         <div class="card">
@@ -169,27 +169,201 @@
         </div>
       </div> -->
 
-      <div class="col-12">
-        <div class="card">
-          <div class="card-body">
-            <p>TODO: INFOBOX</p>
-            <p>Základní údaje jako např:</p>
-            <div class="row">
-              <div class="col-auto">
-                <ul>
-                  <li>Číslo jednotky</li>
-                  <li>Projekt</li>
-                  <li>Lokalita</li>
-                  <li>Dispozice</li>
-                  <li>Bytový dům</li>
-                  <li>Patro</li>
-                </ul>
+      <div class="col-lg-12">
+        <div class="card card-md overflow-hidden">
+          <div class="row row-0">
+            <!-- vizualizace -->
+            <!-- <div class="col-12 col-xl-4">
+              <img src="https://www.central-group.cz/Uloziste/c5/c5048395-8411-40a4-812e-306447b0961f.jpg" class="w-100 h-100 object-cover" alt="">
+            </div> -->
+            <!-- půdorys -->
+            <div class="col-12 col-xl-5">
+              <div class="p-5">
+                <!-- <img src="https://www.central-group.cz/Uloziste/92/92d17a61-24dc-49e0-8271-758e7c374319.gif" class="w-100 h-100 object-contain floor-plan" alt=""> -->
+                <!-- <img src="https://www.central-group.cz/Uloziste/f8/f8312d63-b478-44ab-a0ff-d0ad422ab612.gif" class="w-100 object-contain floor-plan" alt=""> -->
+                <img src="https://www.central-group.cz/Uloziste/1b/1b61efdf-3d70-4d5d-b66a-5abbb7214da1.gif" class="w-100 object-contain floor-plan" alt="">
+              </div>
+            </div>
+            <div class="col-12 col-xl">
+              <div class="card-body p-md-5">
+                <!-- <h2>192-03-147</h2> -->
+                <!-- <p class="text-secondary">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur
+                  perferendis sed suscipit velit vitae voluptatem.
+                </p> -->
+                <div class="datagrid" hidden>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Projekt</div>
+                    <div>Rezidenční čtvrť Tesla Hloubětín</div>
+                  </div>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Lokalita</div>
+                    <div>Praha 9 - Hloubětín</div>
+                  </div>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Bytový dům</div>
+                    <div>A</div>
+                  </div>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Patro</div>
+                    <div>2 NP</div>
+                  </div>
+                  <div class="datagrid-item">
+                    <div class="datagrid-title">Dispozice</div>
+                    <div>3+kk</div>
+                  </div>
+                  <!-- <div class="datagrid-item">
+                    <div class="datagrid-title">Plocha</div>
+                    <div>86 m²</div>
+                  </div> -->
+                </div>
+                <div class="mb-3">
+                  <h2 class="mb-0">Rezidenční čtvrť Tesla Hloubětín</h2>
+                  <h3>Praha 9 - Hloubětín</h3>
+                </div>
+                <!-- <div class="row text-center text-lg-start">
+                  <div class="col">
+                    <div class="datagrid-title">Bytový dům</div>
+                    <div>A</div>
+                  </div>
+                  <div class="col">
+                    <div class="datagrid-title">Patro</div>
+                    <div>2 NP</div>
+                  </div>
+                  <div class="col">
+                    <div class="datagrid-title">Dispozice</div>
+                    <div>3+kk</div>
+                  </div>
+                </div> -->
+                <div class="row gy-3">
+                  <div class="col-4">
+                    <div class="row">
+                      <div class="col-auto d-none d-sm-block">
+                        <span class="avatar">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-building">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M3 21l18 0" />
+                            <path d="M9 8l1 0" />
+                            <path d="M9 12l1 0" />
+                            <path d="M9 16l1 0" />
+                            <path d="M14 8l1 0" />
+                            <path d="M14 12l1 0" />
+                            <path d="M14 16l1 0" />
+                            <path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16" />
+                          </svg>
+                        </span>
+                      </div>
+                      <div class="col">
+                        <div class="text-secondary">Bytový dům</div>
+                        <div>A</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div class="row">
+                      <div class="col-auto d-none d-sm-block">
+                        <span class="avatar">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-stack-2">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 4l-8 4l8 4l8 -4l-8 -4" />
+                            <path d="M4 12l8 4l8 -4" />
+                            <path d="M4 16l8 4l8 -4" />
+                          </svg>
+                        </span>
+                      </div>
+                      <div class="col">
+                        <div class="text-secondary">Patro</div>
+                        <div>2 NP</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div class="row">
+                      <div class="col-auto d-none d-sm-block">
+                        <span class="avatar">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-layout-board">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+                            <path d="M4 9h8" />
+                            <path d="M12 15h8" />
+                            <path d="M12 4v16" />
+                          </svg>
+                        </span>
+                      </div>
+                      <div class="col">
+                        <div class="text-secondary">Dispozice</div>
+                        <div>3+kk</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div class="row">
+                      <div class="col-auto d-none d-sm-block">
+                        <span class="avatar">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-ruler">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M5 4h14a1 1 0 0 1 1 1v5a1 1 0 0 1 -1 1h-7a1 1 0 0 0 -1 1v7a1 1 0 0 1 -1 1h-5a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1" />
+                            <path d="M4 8l2 0" />
+                            <path d="M4 12l3 0" />
+                            <path d="M4 16l2 0" />
+                            <path d="M8 4l0 2" />
+                            <path d="M12 4l0 3" />
+                            <path d="M16 4l0 2" />
+                          </svg>
+                        </span>
+                      </div>
+                      <div class="col">
+                        <div class="text-secondary">Plocha</div>
+                        <div>159 m²</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div class="row">
+                      <div class="col-auto d-none d-sm-block">
+                        <span class="avatar">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-leaf">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M5 21c.5 -4.5 2.5 -8 7 -10" />
+                            <path d="M9 18c6.218 0 10.5 -3.288 11 -12v-2h-4.014c-9 0 -11.986 4 -12 9c0 1 0 3 2 5h3z" />
+                          </svg>
+                        </span>
+                      </div>
+                      <div class="col">
+                        <div class="text-secondary">Energetická třída</div>
+                        <div>B</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div class="row">
+                      <div class="col-auto d-none d-sm-block">
+                        <span class="avatar">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-compass">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M8 16l2 -6l6 -2l-2 6l-6 2" />
+                            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                            <path d="M12 3l0 2" />
+                            <path d="M12 19l0 2" />
+                            <path d="M3 12l2 0" />
+                            <path d="M19 12l2 0" />
+                          </svg>
+                        </span>
+                      </div>
+                      <div class="col">
+                        <div class="text-secondary">Orientace</div>
+                        <div>SV</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-4">
+
+      <div class="col-md-5">
         <div class="card">
           <div class="card-header">
             <h3 class="card-title">Postup předání</h3>
@@ -218,14 +392,12 @@
           </div>
         </div>
       </div>
+      <!-- WIP - Timeline bytu -->
+      <!-- terminy splatnosti vsech faktur; stavebni milniky, klientske terminy (terminove sekce), nastehovani -->
+      <!-- budou tam i uvery pro uverove financovaní -->
       <div class="col">
         <div class="card card-md">
           <div class="card-body">
-            <!-- <ul>
-              <li>timeline</li>
-              <li>terminy splatnosti vsech faktur; stavebni milniky, klientske terminy (terminove sekce), nastehovani</li>
-              <li>budou tam i uvery pro uverove financovaní</li>
-            </ul> -->
             <div class="steps steps-vertical border-0 m-0 p-0 ps-2">
               <div class="step-item">
                 <div>26.4.2026</div>
@@ -265,4 +437,8 @@
 
 </template>
 
-<style scoped></style>
+<style scoped>
+  .floor-plan {
+    max-height: 300px;
+  }
+</style>
