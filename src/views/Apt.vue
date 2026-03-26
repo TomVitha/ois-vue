815fce7077f94a6ed260fe185f7dfbbe2fd13919
Tom Vitha <tomasvitha01@gmail.com>
2026-03-26 11:50:33 +0100

    Handover Procedure section overhaul

--- src/views/Apt.vue
+++ src/views/Apt.vue
@@ -356,7 +356,8 @@
         </div>
       </div>
 
-      <div class="col-lg-5">
+      <!-- * postup předání old -->
+      <!-- <div class="col-lg-5">
         <div class="card">
           <div class="card-header">
             <h3 class="card-title">Postup předání</h3>
@@ -384,7 +385,112 @@
             </ul>
           </div>
         </div>
+      </div> -->
+
+      <!-- WIP -->
+      <div class="col-lg-5">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Postup předání</h3>
+          </div>
+          <div class="card-body">
+            <div class="space-y">
+              <!-- * Completed -->
+              <div class="row g-3">
+                <div class="col-auto">
+                  <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-md text-success icon-tabler icons-tabler-outline icon-tabler-circle-check">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                    <path d="M9 12l2 2l4 -4" />
+                  </svg> -->
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-md text-success icon-tabler icons-tabler-filled icon-tabler-circle-check">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" />
+                  </svg>
+                </div>
+                <div class="col">
+                  <strong>Podpis smlouvy</strong>
+                  <div class="text-secondary">Kupní smlouva byla podepsána všemi stranami.</div>
+                </div>
+              </div>
+              <!-- * Completed -->
+              <div class="row g-3">
+                <div class="col-auto">
+                  <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-md text-success icon-tabler icons-tabler-outline icon-tabler-circle-check">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                    <path d="M9 12l2 2l4 -4" />
+                  </svg> -->
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-md text-success icon-tabler icons-tabler-filled icon-tabler-circle-check">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" />
+                  </svg>
+                </div>
+                <div class="col">
+                  <strong>Úhrada zálohy</strong>
+                  <div class="text-secondary">Záloha ve výši 10 % kupní ceny byla uhrazena.</div>
+                </div>
+              </div>
+              <!-- * Not completed -->
+              <div class="row g-3">
+                <div class="col-auto">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-md text-secondary icons-tabler-outline icon-tabler-circle">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                  </svg>
+                </div>
+                <div class="col">
+                  <strong>Zjišťovací protokol</strong>
+                  <div class="text-secondary">Prohlídka jednotky a sepsání zjišťovacího protokolu.</div>
+                </div>
+              </div>
+              <!-- * Unavailable not completed -->
+              <div class="row g-3 text-secondary">
+                <div class="col-auto">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-md icon-tabler icons-tabler-outline icon-tabler-circle-dashed">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
+                    <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
+                    <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
+                    <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
+                    <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
+                    <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
+                    <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
+                    <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
+                  </svg>
+                </div>
+                <div class="col">
+                  <strong>Doplatek kupní ceny</strong>
+                  <div class="text-secondary">Úhrada zbývající části kupní ceny.</div>
+                  <div class="fs-5">Vyžaduje dokončit: Zjišťovací protokol</div>
+                </div>
+              </div>
+              <!-- * Unavailable not completed -->
+              <div class="row g-3 text-secondary">
+                <div class="col-auto">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-md icon-tabler icons-tabler-outline icon-tabler-circle-dashed">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
+                    <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
+                    <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
+                    <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
+                    <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
+                    <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
+                    <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
+                    <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
+                  </svg>
+                </div>
+                <div class="col">
+                  <strong>Předání klíčů</strong>
+                  <div class="text-secondary">Fyzické předání jednotky a podpis předávacího protokolu.</div>
+                  <div class="fs-5">Vyžaduje dokončit: Doplatek kupní ceny</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       <!-- WIP - Timeline bytu -->
       <!-- terminy splatnosti vsech faktur; stavebni milniky, klientske terminy (terminove sekce), nastehovani -->
       <!-- budou tam i uvery pro uverove financovaní -->
