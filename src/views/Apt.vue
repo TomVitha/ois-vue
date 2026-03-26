e890636cd3124d03d1bc4ae1fe18211dbe22f2e3
Tom Vitha <tomasvitha01@gmail.com>
2026-03-26 13:14:03 +0100

    Add Apartment Timeline

--- src/views/Apt.vue
+++ src/views/Apt.vue
@@ -496,37 +496,231 @@
       <!-- budou tam i uvery pro uverove financovaní -->
       <div class="col">
         <div class="card card-md">
+          <div class="card-header">
+            <h3 class="card-title">Časová osa</h3>
+          </div>
           <div class="card-body">
             <div class="steps steps-vertical border-0 m-0 p-0 ps-2">
+
+              <!-- TODO: Přidat splatnosti faktur (platby k zaplacení) -->
+
+              <!-- TODO: ke klientským termínům přidat odkaz na Žádosti a změny, ať si může provést změny -->
+
+              <div class="step-item inactive">
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Výstavba - crane -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-crane">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M6 21h6" />
+                        <path d="M9 21v-18l-6 6h18" />
+                        <path d="M9 3l10 6" />
+                        <path d="M17 9v4a2 2 0 1 1 -2 2" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">1.6.2022</div>
+                    <div class="fw-semibold">Zahájení výstavby</div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="step-item inactive">
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Klientské termíny - paint -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-paint">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M5 5a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2l0 -2" />
+                        <path d="M19 6h1a2 2 0 0 1 2 2a5 5 0 0 1 -5 5l-5 0v2" />
+                        <path d="M10 16a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -4" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">16.9.2022</div>
+                    <div class="fw-semibold">1. termínová sekce</div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="step-item inactive">
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Klientské termíny - paint -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-paint">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M5 5a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2l0 -2" />
+                        <path d="M19 6h1a2 2 0 0 1 2 2a5 5 0 0 1 -5 5l-5 0v2" />
+                        <path d="M10 16a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -4" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">12.12.2022</div>
+                    <div class="fw-semibold">2. termínová sekce</div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="step-item inactive">
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Klientské termíny - paint -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-paint">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M5 5a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2l0 -2" />
+                        <path d="M19 6h1a2 2 0 0 1 2 2a5 5 0 0 1 -5 5l-5 0v2" />
+                        <path d="M10 16a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -4" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">14.7.2023</div>
+                    <div class="fw-semibold">3. termínová sekce</div>
+                  </div>
+                </div>
+              </div>
+
+
               <div class="step-item">
-                <div>26.4.2026</div>
-                <div class="h2">Zahájení stavby</div>
-                <div class="text-secondary">
-                  popis
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Výstavba - crane -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-crane">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M6 21h6" />
+                        <path d="M9 21v-18l-6 6h18" />
+                        <path d="M9 3l10 6" />
+                        <path d="M17 9v4a2 2 0 1 1 -2 2" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">29.2.2024</div>
+                    <div class="fw-semibold">Dokončení hrubé stavby</div>
+                  </div>
                 </div>
               </div>
-              <div class="step-item active">
-                <div>26.4.2026</div>
-                <div class="h2">Zahájení stavby</div>
-                <div class="text-secondary">
-                  popis
-                </div>
-              </div>
+
               <div class="step-item">
-                <div>26.4.2026</div>
-                <div class="h2">Zahájení stavby</div>
-                <div class="text-secondary">
-                  popis
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Klientské termíny - paint -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-paint">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M5 5a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2l0 -2" />
+                        <path d="M19 6h1a2 2 0 0 1 2 2a5 5 0 0 1 -5 5l-5 0v2" />
+                        <path d="M10 16a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -4" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">30.8.2024</div>
+                    <div class="fw-semibold">4. termínová sekce</div>
+                  </div>
                 </div>
               </div>
+
+
+              <div class="step-item">
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Předání - key -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-key">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M16.555 3.843l3.602 3.602a2.877 2.877 0 0 1 0 4.069l-2.643 2.643a2.877 2.877 0 0 1 -4.069 0l-.301 -.301l-6.558 6.558a2 2 0 0 1 -1.239 .578l-.175 .008h-1.172a1 1 0 0 1 -.993 -.883l-.007 -.117v-1.172a2 2 0 0 1 .467 -1.284l.119 -.13l.414 -.414h2v-2h2v-2l2.144 -2.144l-.301 -.301a2.877 2.877 0 0 1 0 -4.069l2.643 -2.643a2.877 2.877 0 0 1 4.069 0" />
+                        <path d="M15 9h.01" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">25.10.2024</div>
+                    <div class="fw-semibold">Podání prohlášení vlastníka</div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="step-item">
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Výstavba - crane -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-crane">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M6 21h6" />
+                        <path d="M9 21v-18l-6 6h18" />
+                        <path d="M9 3l10 6" />
+                        <path d="M17 9v4a2 2 0 1 1 -2 2" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">31.12.2024</div>
+                    <div class="fw-semibold">Dokončení stavby</div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="step-item">
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Předání - key -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-key">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M16.555 3.843l3.602 3.602a2.877 2.877 0 0 1 0 4.069l-2.643 2.643a2.877 2.877 0 0 1 -4.069 0l-.301 -.301l-6.558 6.558a2 2 0 0 1 -1.239 .578l-.175 .008h-1.172a1 1 0 0 1 -.993 -.883l-.007 -.117v-1.172a2 2 0 0 1 .467 -1.284l.119 -.13l.414 -.414h2v-2h2v-2l2.144 -2.144l-.301 -.301a2.877 2.877 0 0 1 0 -4.069l2.643 -2.643a2.877 2.877 0 0 1 4.069 0" />
+                        <path d="M15 9h.01" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">20.3.2025</div>
+                    <div class="fw-semibold">Kolaudace</div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="step-item">
+                <div class="row">
+                  <div class="col-auto">
+                    <span class="avatar">
+                      <!-- Předání - key -->
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-key">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M16.555 3.843l3.602 3.602a2.877 2.877 0 0 1 0 4.069l-2.643 2.643a2.877 2.877 0 0 1 -4.069 0l-.301 -.301l-6.558 6.558a2 2 0 0 1 -1.239 .578l-.175 .008h-1.172a1 1 0 0 1 -.993 -.883l-.007 -.117v-1.172a2 2 0 0 1 .467 -1.284l.119 -.13l.414 -.414h2v-2h2v-2l2.144 -2.144l-.301 -.301a2.877 2.877 0 0 1 0 -4.069l2.643 -2.643a2.877 2.877 0 0 1 4.069 0" />
+                        <path d="M15 9h.01" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="col">
+                    <div class="text-secondary">28.2.2025</div>
+                    <div class="fw-semibold">Nastěhování</div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
       </div>
+
+
       <div class="col-12">
         <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Galerie</h3>
+          </div>
           <div class="card-body">
-            TODO: Galerie
+            <!-- TODO: Galerie -->
           </div>
         </div>
       </div>
@@ -537,7 +731,17 @@
 </template>
 
 <style scoped>
-  .floor-plan {
+
+  /* .floor-plan {
     max-height: 300px;
+  } */
+  /* WIP */
+  .step-item.inactive {
+    color: var(--tblr-disabled-color);
+  }
+
+  /* * Custom class .inactive */
+  .step-item.inactive:after, .step-item.inactive:before {
+    background: var(--tblr-steps-inactive-color);
   }
 </style>
