ea8545a14aa4b72263eec84fc05c783b8fb76860
Tom Vitha <tomasvitha01@gmail.com>
2026-05-18 08:57:40 +0200

    Unify Dropzones

--- src/views/DOIS/DetailObjednavky.vue
+++ src/views/DOIS/DetailObjednavky.vue
@@ -985,24 +985,18 @@
 
           <div class="modal-body">
             <form class="dropzone dz-clickable" id="dropzone-order-attachments" action="./" autocomplete="off" novalidate>
-              <div class="dz-default dz-message">
+              <div class="dz-default dz-message m-0">
                 <button class="dz-button" type="button">
-                  <span class="d-none d-lg-inline">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-drag-drop">
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M19 11v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
-                      <path d="M13 13l9 3l-4 2l-2 4l-3 -9" />
-                      <path d="M3 3l0 .01" />
-                      <path d="M7 3l0 .01" />
-                      <path d="M11 3l0 .01" />
-                      <path d="M15 3l0 .01" />
-                      <path d="M3 7l0 .01" />
-                      <path d="M3 11l0 .01" />
-                      <path d="M3 15l0 .01" />
-                    </svg>
-                    Přetáhněte soubory nebo kliknutím vyberte
-                  </span>
-                  <span class="d-lg-none">Kliknutím vyberte soubory k nahrání</span>
+                  <Empty title="Přetáhněte soubory" subtitle="nebo kliknutím vyberte">
+                    <template #icon>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud-upload">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7h-1" />
+                        <path d="M9 15l3 -3l3 3" />
+                        <path d="M12 12l0 9" />
+                      </svg>
+                    </template>
+                  </Empty>
                 </button>
               </div>
             </form>
--- src/views/DOIS/NahraniSouboru.vue
+++ src/views/DOIS/NahraniSouboru.vue
@@ -41,28 +41,6 @@
             </div>
 
             <form class="dropzone dz-clickable" id="dropzone-file-upload" action="./" autocomplete="off" novalidate>
-              <!-- * Default markup -->
-              <!-- <div class="dz-default dz-message">
-                <button class="dz-button" type="button">
-                  <span class="d-none d-lg-inline">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-drag-drop">
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M19 11v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
-                      <path d="M13 13l9 3l-4 2l-2 4l-3 -9" />
-                      <path d="M3 3l0 .01" />
-                      <path d="M7 3l0 .01" />
-                      <path d="M11 3l0 .01" />
-                      <path d="M15 3l0 .01" />
-                      <path d="M3 7l0 .01" />
-                      <path d="M3 11l0 .01" />
-                      <path d="M3 15l0 .01" />
-                    </svg>
-                    Přetáhněte soubory nebo kliknutím vyberte
-                  </span>
-                  <span class="d-lg-none">Kliknutím vyberte soubory k nahrání</span>
-                </button>
-              </div> -->
-              <!-- * Custom markup (utilizing our Empty component) -->
               <div class="dz-default dz-message m-0">
                 <button class="dz-button" type="button">
                   <Empty title="Přetáhněte soubory" subtitle="nebo kliknutím vyberte">
