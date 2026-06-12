de3b76a5da41facaa3f303808a41777788e979c5
Tom Vitha <tomasvitha01@gmail.com>
2026-06-12 13:27:37 +0200

    Add option to "reference" individual document when adding comment

--- src/components/DOIS/OrderAddComment.vue
+++ src/components/DOIS/OrderAddComment.vue
@@ -63,25 +63,54 @@
 </script>
 
 <template>
-  <div class="row g-2 align-items-end">
-    <div class="col">
-      <textarea
-        v-model="newCommentText"
-        :name="`comment-${props.target}-${props.targetId}`"
-        :id="`comment-${props.target}-${props.targetId}`"
-        class="form-control"
-        placeholder="Napište komentář..."
-        @keydown.ctrl.enter="submitComment"
-        style="resize: none; field-sizing: content;"></textarea>
-    </div>
-    <div class="col-auto">
-      <!-- * Add comment to document -->
+  <div class="position-relative">
+    <textarea
+      v-model="newCommentText"
+      :name="`comment-${props.target}-${props.targetId}`"
+      :id="`comment-${props.target}-${props.targetId}`"
+      class="form-control"
+      placeholder="Napište komentář..."
+      @keydown.enter.exact.prevent="submitComment"
+      style="resize: none; field-sizing: content; padding-bottom: 3.5rem"></textarea>
+    <div class="d-flex align-items-center position-absolute end-0 bottom-0 px-2 pb-2 w-100">
+      <!-- WIP -->
+      <div class="btn-actions">
+        <div class="dropdown">
+          <button class="btn btn-action dropdown-toggle" data-bs-toggle="dropdown" draggable="false">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-plus">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+              <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2" />
+              <path d="M12 11l0 6" />
+              <path d="M9 14l6 0" />
+            </svg>
+          </button>
+          <div class="dropdown-menu">
+            <button class="dropdown-item">hlavni-dokument-1.pdf</button>
+            <button class="dropdown-item">nejaky-dokument-1.pdf</button>
+            <button class="dropdown-item">dalsi-dokument-2.pdf</button>
+          </div>
+        </div>
+      </div>
+      <!-- NOTE: Pokud se vybere specifický dokument, ke kterému se komentář pojí -->
+      <span class="tag">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-text">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+          <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2" />
+          <path d="M9 9l1 0" />
+          <path d="M9 13l6 0" />
+          <path d="M9 17l6 0" />
+        </svg>
+        nazev-dokumentu.pdf
+        <a href="#" class="btn-close"></a>
+      </span>
       <button
-        class="btn btn-primary"
+        class="btn btn-primary ms-auto"
         :class="{ 'btn-icon': !isDesktop }"
         :disabled="!canSubmitComment"
         @click="submitComment">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-send">
+        <svg v-if="!isDesktop" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-send">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M10 14l11 -11" />
           <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
