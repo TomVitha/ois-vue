90849f10b1eb14a12dd3b7bda725fe12556a25ff
Tom Vitha <tomasvitha01@gmail.com>
2026-06-12 13:43:03 +0200

    Fix theme switch tooltip position breaking

--- src/components/Nav/NavbarNav.vue
+++ src/components/Nav/NavbarNav.vue
@@ -10,13 +10,13 @@
     <div class="d-flex align-items-center">
 
       <!-- Theme switch -->
-      <div class="nav-item">
-        <button class="nav-link px-0 hide-theme-dark" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Zapnout tmavý režim" @click.prevent="themeStore.applyTheme('dark')">
+      <div class="nav-item" data-bs-toggle="tooltip" data-bs-placement="bottom" :title="themeStore.theme === 'dark' ? 'Zapnout světlý režim' : 'Zapnout tmavý režim'">
+        <button class="nav-link px-0 hide-theme-dark" @click.prevent="themeStore.applyTheme('dark')">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
             <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
           </svg>
         </button>
-        <button class="nav-link px-0 hide-theme-light" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Zapnout světlý režim" @click.prevent="themeStore.applyTheme('light')">
+        <button class="nav-link px-0 hide-theme-light" @click.prevent="themeStore.applyTheme('light')">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
             <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
             <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
